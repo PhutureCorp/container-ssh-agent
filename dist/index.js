e3fc9ce932579a5d027d24fc6b7e80131ae83bc6
Adam Rogas <adam@rogassi.com>
2020-12-04 16:41:08 -0800

    testing changes

--- dist/index.js
+++ dist/index.js
@@ -121,7 +121,7 @@ const fs = __webpack_require__(747);
 
 try {
 
-    const home = process.env['HOME'];
+    const home = '/root'//process.env['HOME'];
     const homeSsh = home + '/.ssh';
 
     const privateKey = core.getInput('ssh-private-key');
